4ee51f6665255872e8bfb44f44d3211b68fc0238
Pritam <pritamk6284987295@gmail.com>
2025-10-16 15:23:02 +0530

    frontend-deploy-issue

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.BACKEND_URL ?? 'http://localhost:5001'}/api`,
+    baseURL: `${import.meta.env.VITE_BACKEND_URL ?? 'http://localhost:5001'}/api`,
     withCredentials: true, // send cookies
 })
